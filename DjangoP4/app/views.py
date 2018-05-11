deb018108eb61565f948544fd0206f001dbbabe0
Matthew Lin <mlin367@yahoo.com>
2018-05-10 19:46:39 -0700

    'first'

---
+++ DjangoP4/app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context_dict = {'text':'hello world', 'number':180}
+    return render(request, 'app/index.html', context_dict)
+
+def other(request):
+    return render(request, 'app/other.html')
+
+def relative(request):
+    return render(request, 'app/relative_url_templates.html')
